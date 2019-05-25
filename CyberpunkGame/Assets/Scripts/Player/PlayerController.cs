6eece3faf9ea504bb897d4186bae1a7d74c6ea09
LeeXx <leoslexus@gmail.com>
2019-05-25 01:49:05 +0100

    update

--- CyberpunkGame/Assets/Scripts/Player/PlayerController.cs
+++ CyberpunkGame/Assets/Scripts/Player/PlayerController.cs
@@ -22,7 +22,7 @@ public class PlayerController : MonoBehaviour
         playerMovements = GetComponent<PlayerMovements>();
     }
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if(Input.GetMouseButton(0))
         {
