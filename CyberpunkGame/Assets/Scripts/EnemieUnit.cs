9dbf9ab119557ed6beb7db06de18c0dbb698c238
LeeXx <leoslexus@gmail.com>
2019-05-29 23:35:20 +0100

    RobotV1

--- CyberpunkGame/Assets/Scripts/EnemieUnit.cs
+++ CyberpunkGame/Assets/Scripts/EnemieUnit.cs
@@ -1,18 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 public class EnemieUnit : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private bool isPatrolling;
+    public List<Transform> patrolPositions;
+    [SerializeField] private float patrolTimer;
+    public NavMeshAgent agent;
     void Start()
     {
-        
+        Invoke("Patrol", patrolTimer);
     }
 
-    // Update is called once per frame
+    IEnumerator StartPatrolling()
+    {
+        for (int i = 0; i < patrolPositions.Count; i++)
+        {
+            agent.SetDestination(patrolPositions[i].position);
+            if(agent.remainingDistance <= agent.stoppingDistance)
+            {
+                yield return new WaitForSeconds(patrolTimer);
+            }
+ 
+        }
+
+
+
+    }
     void Update()
     {
-        
+
     }
 }
