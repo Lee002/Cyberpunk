206f54b7ee5812cb6205a55f81303f86932ade5d
iwanPlays <iwanplaystube@gmail.com>
2019-05-29 20:06:38 +0200

    level change script

---
+++ CyberpunkGame/Assets/Scripts/NextScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour {
+    
+	void LoadScene(int scene)
+    {
+        SceneManager.LoadScene(scene); // starts at 0, see File/Build Settings
+    }
+	
+    void LoadSceneNext()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
