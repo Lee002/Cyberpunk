0c62b32fbe758e3c176887918b935ec2eb5904e1
iwanPlays <iwanplaystube@gmail.com>
2019-05-30 21:17:43 +0200

    start game button script

---
+++ CyberpunkGame/Assets/Scripts/UiPlayButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiPlayButton : MonoBehaviour
+{
+    private bool Clicked = false;
+
+    public GameObject NextSceneObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!Clicked)
+        {
+            Button b = GetComponent<Button>();
+            AudioSource audio = GetComponent<AudioSource>();
+            b.onClick.AddListener(delegate ()
+            {
+                audio.Play();
+                NextSceneObj.GetComponent<NextScene>().LoadSceneNext();
+                Clicked = true;
+            });
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
