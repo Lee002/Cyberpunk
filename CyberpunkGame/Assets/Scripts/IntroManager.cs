1a367aff6e8bac209e9cd4701b41e603e091e879
LeeXx <leoslexus@gmail.com>
2019-05-30 13:25:46 +0100

    Revert "Merge branch 'master' of https://github.com/Lee002/Cyberpunk"
    This reverts commit a4e503e9cb0db45dd3c9981a101e5d419d233fd7, reversing
    changes made to 0ad7811b368ed137770d17e9299f4eba542a5d79.

--- CyberpunkGame/Assets/Scripts/IntroManager.cs
+++ CyberpunkGame/Assets/Scripts/IntroManager.cs
@@ -6,51 +6,32 @@ public class IntroManager : MonoBehaviour
 {
     public AudioSource droidVoice1;
     public List<AudioClip> droidVoices = new List<AudioClip>();
-    public GameObject levelEndObj;
-
     public void StartAudio()
     {
         droidVoice1.Play();
-        Invoke("StartAudio2", 2f);
+        Invoke("StartAudio2", 7f);
     }
     void StartAudio2()
     {
-
+        
         droidVoice1.clip = droidVoices[0];
         droidVoice1.Play();
-        Invoke("StartAudio3", 3f);
+        Invoke("StartAudio3", 5f);
     }
 
     void StartAudio3()
     {
         droidVoice1.clip = droidVoices[1];
         droidVoice1.Play();
-        Invoke("StartAudio4", 3f);
+        Invoke("StartAudio4", 6f);
     }
     void StartAudio4()
     {
         droidVoice1.clip = droidVoices[2];
         droidVoice1.Play();
-        Invoke("StartAudio5", 4f);
-    }
-    void StartAudio5()
-    {
-        droidVoice1.clip = droidVoices[3];
-        droidVoice1.Play();
-        Invoke("StartAudio6", 2f);
-    }
-    void StartAudio6()
-    {
-        droidVoice1.clip = droidVoices[4];
-        droidVoice1.Play();
-        Invoke("StartFade", 4f);
-    }
-    void StartFade()
-    {
-        levelEndObj.GetComponent<NextScene>().LoadSceneNext();
     }
     void Start()
     {
-        Invoke("StartAudio", 9.5f);
+        Invoke("StartAudio", 14f);
     }
 }
