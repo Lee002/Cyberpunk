13dab86446693741aec84dcdbdef5995d2052db7
LeeXx <leoslexus@gmail.com>
2019-05-30 13:33:07 +0100

    c

--- CyberpunkGame/Assets/Scripts/NextScene.cs
+++ CyberpunkGame/Assets/Scripts/NextScene.cs
@@ -3,18 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+<<<<<<< HEAD
 public class NextScene  {
 
 
     
 	public void LoadScene(int scene)
+=======
+
+public class NextScene : MonoBehaviour {
+
+    public Animator fadeAnimator;
+
+    public int sceneToLoad;
+
+	public void LoadScene(int scene)
+
+    {
+        sceneToLoad = scene;
+        fadeAnimator.SetTrigger("FadeToBlack");
+    }
+
+    public void OnFadeComplete()
+>>>>>>> parent of a364d89... Revert "fix"
     {
         SceneManager.LoadScene(scene); // starts at 0, see File/Build Settings
     }
 	
     public void LoadSceneNext()
     {
+<<<<<<< HEAD
         LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+=======
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+>>>>>>> parent of a364d89... Revert "fix"
     }
 
     public IEnumerator  LoadSceneNextAfterTime()
